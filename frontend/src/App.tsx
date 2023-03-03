061ef469a85849cfbb617615ae537de0f24ed534
Philipp Mandler <info@philipp-mandler.com>
2023-03-03 15:56:22 +0100

    Setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="p-10 border-2 border-black rounded-xl shadow-brutal">
+        <h1 className="text-5xl font-medium">Welcome to transcribee! 🐝</h1>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        brutal: '5px 5px #000',
+      },
+    },
+    fontFamily: {
+      sans: [
+        'Inter',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Noto Sans',
+        'Helvetica',
+        'Arial',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+      ],
+    },
+  },
+  plugins: [],
+};
