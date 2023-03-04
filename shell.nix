f11b6534960829c0368a5f7af9969c66e5beb68c
Jaro Habiger <jarohabiger@googlemail.com>
2023-03-04 19:46:52 +0100

    🔧 add vscode settings.json
    even if it is a bit haram, its still very useful for our specific application since we manage most of the environment anyways

--- shell.nix
+++ shell.nix
@@ -4,8 +4,22 @@ pkgs.mkShell {
     pre-commit
 
     python310
+    python310Packages.black
     pdm
 
     nodePackages.pnpm
-  ];
+
+  ] ++
+
+  # we need these to build whisper-cpp (+ python bindings)
+  [
+    stdenv
+  ] ++ (if !stdenv.isDarwin then [ ] else [
+    darwin.apple_sdk.frameworks.Accelerate
+  ]);
+
+  shellHook = ''
+    export CFLAGS="-I ${pkgs.libcxx.dev}/include/c++/v1 -I${pkgs.openfst}/include $CFLAGS"
+    export LDFLAGS="-L${pkgs.openfst}/lib $LDFLAGS"
+  '';
 }
