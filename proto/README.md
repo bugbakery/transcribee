e61e0ba825ae47685f63754912aaec6f9630b296
Jaro Habiger <jarohabiger@googlemail.com>
2023-03-04 01:31:00 +0100

    add proto directory for common python code

---
+++ proto/README.md
@@ -0,0 +1,3 @@
+# transcribee-proto
+
+This folder contains datatypes that are common between the backend and the worker python modules.
---
+++ proto/document.py
@@ -0,0 +1,16 @@
+from typing import Literal, List
+from pydantic import BaseModel
+
+class TranscriptSegment(BaseModel):
+    text: str
+    start: float
+    end: float
+    conf: float
+
+class Paragraph(BaseModel):
+    type: Literal['paragraph'] = 'paragraph'
+    speaker: str
+    children: List[TranscriptSegment]
+
+class Document(BaseModel):
+    __root__: List[Paragraph]
