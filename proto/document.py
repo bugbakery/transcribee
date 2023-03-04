d1053951713ef2c93f888b7776cd50a004179214
Jaro Habiger <jarohabiger@googlemail.com>
2023-03-04 02:34:11 +0100

    add pre-commit config
    also fix errors found

--- proto/document.py
+++ proto/document.py
@@ -1,5 +1,7 @@
-from typing import Literal, List
+from typing import List, Literal
+
 from pydantic import BaseModel
+
 
 class TranscriptSegment(BaseModel):
     text: str
@@ -7,10 +9,12 @@ class TranscriptSegment(BaseModel):
     end: float
     conf: float
 
+
 class Paragraph(BaseModel):
-    type: Literal['paragraph'] = 'paragraph'
+    type: Literal["paragraph"] = "paragraph"
     speaker: str
     children: List[TranscriptSegment]
+
 
 class Document(BaseModel):
     __root__: List[Paragraph]
