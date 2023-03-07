ef8ff4c8525a9b358f5549a70d7d97b25bd19d77
pajowu <git@ca.pajowu.de>
2023-03-07 15:51:36 +0100

    🐛 Fix admin registration

--- backend/transcribee_backend/base/admin.py
+++ backend/transcribee_backend/base/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Document, DocumentUpdate, User
+from .models import Document, DocumentUpdate, Task, User, Worker
 
-admin.register(User)
-admin.register(Document)
-admin.register(DocumentUpdate)
+admin.site.register(User)
+admin.site.register(Document)
+admin.site.register(DocumentUpdate)
+
+admin.site.register(Worker)
+admin.site.register(Task)
