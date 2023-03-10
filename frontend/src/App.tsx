a650ed7bbef4dca8ada29e884973100480a20af7
Philipp Mandler <info@philipp-mandler.com>
2023-03-10 19:38:18 +0100

    Add page not found page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import DocumentPage from './pages/DocumentPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import NewDocumentPage from './pages/NewDocumentPage';
+import PageNotFoundPage from './pages/PageNotFoundPage';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/new" component={NewDocumentPage} />
         <Route path="/login" component={LoginPage} />
         <Route path="/document" component={DocumentPage} />
+        <Route component={PageNotFoundPage} />
       </Switch>
     </div>
   );
