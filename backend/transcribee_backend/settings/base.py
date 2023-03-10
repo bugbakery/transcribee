56b76b3d3524ea332d67410e867dfdc86ca22b03
pajowu <git@ca.pajowu.de>
2023-03-10 20:58:51 +0100

    🔧 Put token auth first
    Otherwise we get CRSF errors with a user is both logged in via session auth and provides a token

--- backend/transcribee_backend/settings/base.py
+++ backend/transcribee_backend/settings/base.py
@@ -119,9 +119,9 @@ class Base(Configuration):
 
     REST_FRAMEWORK = {
         "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.TokenAuthentication",
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
-            "rest_framework.authentication.TokenAuthentication",
         ]
     }
 
