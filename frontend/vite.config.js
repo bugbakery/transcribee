374e81c48ab80f4dddcf0afea3739aedccc91189
Philipp Mandler <info@philipp-mandler.com>
2023-03-10 20:38:10 +0100

    Clean up vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        headers: {
-          Origin: "http://localhost:8000"
-        }
-      },
+      '/api': 'http://localhost:8000',
     },
   },
 });
