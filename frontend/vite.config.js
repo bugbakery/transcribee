8d6039a8406fac8606fa6c71d4ae77c669b39f8a
Jaro Habiger <jarohabiger@googlemail.com>
2023-03-12 17:27:19 +0100

    🐛 frontend: always use ipv4 in vite reverse-proxy
    When localhost resolves to ipv6 the reverse-proxying previously
    didn't work because django always binds to ipv4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'http://127.0.0.1:8000',
     },
   },
 });
