f88ee0bd8e604e98086004764968c7ca1a4e16ed
pajowu <git@ca.pajowu.de>
2023-03-12 14:45:31 +0100

    🧑‍💻 Pin nixpks in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,14 @@
-{ pkgs ? import <nixpkgs> { } }:
+let
+  # We use a fixed version of nixpkgs here, so we get a recent enough version of
+  # pdm (2.4.6 at the time of writing). The version shipped in current (as of 12-03-23)
+  # nixpkgs-stable does not support dependencies with relative paths
+  pkgs = import
+    (fetchTarball {
+      name = "nixpkgs-unstable-new-enough-pdm";
+      url = "https://github.com/NixOS/nixpkgs/archive/43862987c3cf2554a542c6dd81f5f37435eb1423.tar.gz";
+    })
+    { };
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     pre-commit
