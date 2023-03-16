4e19a61cd9e36318c87eccf3284a222947901979
pajowu <git@ca.pajowu.de>
2023-03-16 16:14:24 +0100

    🔧 Enable atomic requests in dev settings

--- backend/transcribee_backend/settings/dev.py
+++ backend/transcribee_backend/settings/dev.py
@@ -11,6 +11,7 @@ class Dev(Base):
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
+            "ATOMIC_REQUESTS": True,
         }
     }
 
