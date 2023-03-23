af3c0e605752d38268f4e53d31a4608a1c8c7a1c
Philipp Mandler <info@philipp-mandler.com>
2023-03-23 02:26:27 +0100

    Connect slate to automerge

--- frontend/src/editor/automerge_websocket_provider.ts
+++ frontend/src/editor/automerge_websocket_provider.ts
@@ -1,4 +1,5 @@
 import { Observable } from 'lib0/observable';
+import { Change } from '@automerge/automerge';
 
 enum MessageSyncType {
   Change = 1,
@@ -18,6 +19,11 @@ export class AutomergeWebsocketProvider extends Observable<
     this.url = url;
 
     this.connectWebsocket();
+
+    this.on('update', ({ change, remote }: { change: Change; remote: boolean }) => {
+      if (remote) return;
+      this.ws.send(change);
+    });
   }
 
   connectWebsocket() {
@@ -32,7 +38,7 @@ export class AutomergeWebsocketProvider extends Observable<
       const msg_type = msg_data[0];
       const msg = msg_data.slice(1);
       if (msg_type === MessageSyncType.Change) {
-        this.emit('update', [msg]);
+        this.emit('update', [{ change: msg, remote: true }]);
       } else if (msg_type === MessageSyncType.ChangeBacklogComplete) {
         this.emit('initalSyncComplete', []);
         console.log('All changes synced');
