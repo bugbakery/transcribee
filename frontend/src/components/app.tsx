9219ebf7858c7d0205d43a3d4f64ce5a9686277c
Jaro Habiger <jarohabiger@googlemail.com>
2023-03-30 22:34:12 +0200

    🧹 factor out AppCenter component

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -1,3 +1,7 @@
 import { primitiveWithClassname } from '../styled';
 
 export const AppContainer = primitiveWithClassname('div', 'max-w-screen-xl p-6 mx-auto');
+export const AppCenter = primitiveWithClassname(
+  'div',
+  'h-screen p-6 flex items-center justify-center',
+);
--- frontend/src/pages/page_not_found.tsx
+++ frontend/src/pages/page_not_found.tsx
@@ -1,15 +1,16 @@
 import { useLocation } from 'wouter';
+import { AppCenter } from '../components/app';
 import { PrimaryButton } from '../components/button';
 
 export function PageNotFoundPage() {
   const [_, navigate] = useLocation();
   return (
-    <div className="h-screen p-6 flex items-center justify-center">
+    <AppCenter>
       <div className="flex flex-col items-center gap-6">
         <h1 className="font-medium text-4xl">Page Not Found</h1>
         <p>We couldn&apos;t find what you were looking for.</p>
         <PrimaryButton onClick={() => navigate('/')}>Go to Home</PrimaryButton>
       </div>
-    </div>
+    </AppCenter>
   );
 }
