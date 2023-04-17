d03c6f12bf3fbc65099d06b0964709a93cc77bbb
pajowu <git@ca.pajowu.de>
2023-04-17 13:15:38 +0200

    ✨ Add webvtt export

---
+++ frontend/src/utils/export/webvtt.ts
@@ -0,0 +1,25 @@
+import { WebVtt, VttCue } from '@audapolis/webvtt-writer';
+import { Document } from '../../editor/types';
+
+export function generateWebVtt(doc: Document): WebVtt {
+  const vtt = new WebVtt(
+    'This file was generated using transcribee. Find out more at https://github.com/transcribee/transcribee',
+  );
+  for (const paragraph of doc.paragraphs) {
+    if (paragraph.children.length === 0) {
+      continue;
+    }
+    const start = paragraph.children[0].start;
+    const end = paragraph.children[paragraph.children.length - 1].end;
+    if (start === undefined || end === undefined) {
+      continue;
+    }
+    const cue = new VttCue({
+      startTime: start / 1e3,
+      endTime: end / 1e3,
+      payload: paragraph.children.map((x) => x.text).join(''),
+    });
+    vtt.add(cue);
+  }
+  return vtt;
+}
