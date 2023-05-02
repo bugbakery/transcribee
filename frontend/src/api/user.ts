80dcbfa9a7bbc50dd7fe168da1ab87b7deaca03f
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2023-05-02 15:30:15 +0200

    do not retry getMe, when receiving 422

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,4 +1,6 @@
+import { defaultConfig } from 'swr/_internal';
 import { fetcher, makeSwrHook } from '../api';
+import { SWRConfiguration } from 'swr';
 
 export const login = fetcher
   .path('/api/v1/users/login/')
@@ -6,4 +8,16 @@ export const login = fetcher
   .create();
 export const getMe = fetcher.path('/api/v1/users/me/').method('get').create();
 
-export const useGetMe = makeSwrHook('getMe', getMe);
+const useGetMeWithRetry = makeSwrHook('getMe', getMe);
+
+export const useGetMe = (
+  params: Parameters<typeof useGetMeWithRetry>[0],
+  options?: Partial<SWRConfiguration>,
+) =>
+  useGetMeWithRetry(params, {
+    onErrorRetry: (err, key, config, revalidate, opts) => {
+      if (err.status === 422) return;
+      else defaultConfig.onErrorRetry(err, key, config, revalidate, opts);
+    },
+    ...options,
+  });
