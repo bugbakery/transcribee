ff79fcea6510c7f919ea49d719cf4d3a0477d0c5
Jaro Habiger <jarohabiger@googlemail.com>
2023-05-26 04:13:22 +0200

    💅 add sticky speakers on large screens

--- frontend/src/editor/transcription_editor.tsx
+++ frontend/src/editor/transcription_editor.tsx
@@ -16,6 +16,7 @@ import { CSSProperties, ComponentProps, useContext, useCallback, memo } from 're
 import { SpeakerColorsContext, SpeakerColorsProvider } from './speaker_colors';
 import { useMediaQuery } from '../utils/use_media_query';
 import { createPortal } from 'react-dom';
+import { getSpeakerName } from '../utils/document';
 
 export function formattedTime(sec: number | undefined): string {
   if (sec === undefined) {
@@ -43,14 +44,15 @@ function renderElement({ element, children, attributes }: RenderElementProps): J
 
   const editor = useSlateStatic();
   const idx = ReactEditor.findPath(editor, element)[0];
-  let currentSpeakerEndIdx = 0;
+  let speakerEndIdx = 0;
   for (
-    currentSpeakerEndIdx = idx;
-    currentSpeakerEndIdx < editor.doc.children.length;
-    currentSpeakerEndIdx++
-  ) {
-    if (editor.doc.children[currentSpeakerEndIdx].speaker != element.speaker) break;
-  }
+    speakerEndIdx = idx;
+    speakerEndIdx < editor.doc.children.length &&
+    editor.doc.children[speakerEndIdx].speaker == element.speaker;
+    speakerEndIdx++
+  );
+  speakerEndIdx--;
+  const speakerChanged = idx == 0 || editor.doc.children[idx - 1].speaker != element.speaker;
 
   let portalNode = document.getElementById('meta-portal');
   if (portalNode == null) {
@@ -71,7 +73,7 @@ function renderElement({ element, children, attributes }: RenderElementProps): J
       style={
         {
           '--element-idx': idx,
-          '--current-speaker-end-idx': currentSpeakerEndIdx,
+          '--current-speaker-end-idx': speakerEndIdx,
         } as CSSProperties
       }
     >
@@ -84,7 +86,7 @@ function renderElement({ element, children, attributes }: RenderElementProps): J
         className={clsx(
           'w-2 mr-2 h-full rounded-md',
           'row-start-[calc(var(--element-idx)*3+1)]',
-          'row-end-[calc(var(--current-speaker-end-idx)*3)]',
+          'row-end-[calc(var(--current-speaker-end-idx)*3+3)]',
           'col-start-2',
           'md:col-start-3',
           'xl:mr-4',
@@ -97,6 +99,8 @@ function renderElement({ element, children, attributes }: RenderElementProps): J
         className={clsx(
           `text-slate-500 dark:text-neutral-400 font-mono`,
           'row-start-[calc(var(--element-idx)*3+1)] col-start-3',
+          speakerChanged && 'md:row-start-[calc(var(--element-idx)*3+2)]',
+          'xl:row-start-[calc(var(--element-idx)*3+1)]',
           'md:col-start-2',
           'xl:col-start-1 xl:mr-4',
         )}
@@ -105,24 +109,49 @@ function renderElement({ element, children, attributes }: RenderElementProps): J
         {formattedTime(startAtom.start)}
       </div>
 
+      {/* speaker names */}
+      {speakerChanged && (
+        <div
+          className={clsx(
+            'sticky top-0',
+            '-mt-[0.1rem] py-1 mr-1',
+            'max-w-none break-all text-neutral-400',
+            'text-sm font-semibold',
+            'md:max-w-[200px] md:text-neutral-600 xl:text-right',
+            'hidden md:block bg-white',
+            'row-start-[calc(var(--element-idx)*3+1)] col-start-2',
+            'xl:row-start-[calc(var(--element-idx)*3+1)]',
+            'xl:pr-3',
+          )}
+        >
+          {getSpeakerName(element.speaker, editor.doc.speaker_names)}
+        </div>
+      )}
       <SpeakerDropdown
         contentEditable={false}
         paragraph={element}
         buttonClassName={clsx(
           'max-w-none break-all text-neutral-400',
-          'md:max-w-[200px] md:text-neutral-600',
+          'md:max-w-[200px] md:text-neutral-600 xl:text-right',
           'xl:pr-2',
+          'md:opacity-0 md:hover:opacity-100',
         )}
         className={clsx(
           'mx-2',
           'row-start-[calc(var(--element-idx)*3+1)] col-start-4',
-          'md:row-start-[calc(var(--element-idx)*3+2)] md:col-start-2 md:-ml-2',
+          'md:col-start-2 md:-ml-2',
           'xl:row-start-[calc(var(--element-idx)*3+1)]',
         )}
       />
 
       {/* helper for bottom padding */}
-      <div className={clsx('mb-6 md:mb-2 xl:mb-3', 'row-start-[calc(var(--element-idx)*3+3)]')} />
+      <div
+        className={clsx(
+          'mb-6 md:mb-0 xl:mb-3',
+          'row-start-[calc(var(--element-idx)*3+3)]',
+          idx == speakerEndIdx && 'md:mb-2',
+        )}
+      />
     </div>
   );
 
@@ -136,7 +165,8 @@ function renderElement({ element, children, attributes }: RenderElementProps): J
         className={clsx(
           `col-start-2 col-span-2`,
           'row-start-[calc(var(--element-idx)*3+2)] col-start-3',
-          'md:row-start-[calc(var(--element-idx)*3+1)] md:row-span-1 md:col-start-4 md:col-span-1',
+          'md:row-start-[calc(var(--element-idx)*3+1)] md:row-span-2 md:col-start-4 md:col-span-1',
+          'md:mb-2',
         )}
         lang={element.lang}
         spellCheck={false}
