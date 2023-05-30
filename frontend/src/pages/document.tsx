25f59e3ac5f9bc17714ea08e190eccfdc546f132
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2023-05-30 12:52:55 +0200

    frontend: hide unloaded document titles

--- frontend/src/pages/document.tsx
+++ frontend/src/pages/document.tsx
@@ -28,6 +28,10 @@ const LazyDebugPanel = lazy(() =>
 function DocumentTitle({ name, onChange }: { name: string; onChange: (newTitle: string) => void }) {
   const [editable, setEditable] = useState(false);
 
+  if (name == null || name == undefined) {
+    return <></>;
+  }
+
   if (editable) {
     return (
       <form
