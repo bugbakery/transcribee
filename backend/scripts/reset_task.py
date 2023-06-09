44b8ff45957b85358a2b14b4501fdef3e57cd429
pajowu <git@ca.pajowu.de>
2023-06-09 15:26:36 +0200

    🧑‍💻 Add script to reset failed task(s)

---
+++ backend/scripts/reset_task.py
@@ -0,0 +1,24 @@
+import argparse
+import uuid
+
+from sqlmodel import or_, update
+from transcribee_backend.config import settings
+from transcribee_backend.db import get_session
+from transcribee_backend.models.task import Task, TaskState
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--uuid", required=True, type=uuid.UUID, help="Task UUID or Document UUID"
+    )
+    args = parser.parse_args()
+    session = next(get_session())
+    task = session.exec(
+        update(Task)
+        .where(
+            or_(Task.id == args.uuid, Task.document_id == args.uuid),
+            Task.state == TaskState.FAILED,
+        )
+        .values(state=TaskState.NEW, remaining_attempts=settings.task_attempt_limit)
+    )
+    session.commit()
