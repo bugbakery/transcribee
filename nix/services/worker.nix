b1fa533d8d6621ab83d4f47b02b36b2163e46015
Philipp Mandler <info@philipp-mandler.com>
2023-12-22 16:28:58 +0100

    Fix worker service

--- nix/services/worker.nix
+++ nix/services/worker.nix
@@ -18,49 +18,42 @@ in
     };
   };
 
-  config = mkIf cfg.enable
-    (mkMerge [
-      (optionalAttrs (hasAttr "systemd" options) {
-        systemd.services.transcribee-worker = {
-          enable = true;
-          unitConfig = {
-            Type = "simple";
-            Restart = "always";
-          };
-          script = ''
-            echo ${pkgs.ffmpeg.bin}
-              ${pkgs.pdm}/bin/pdm run -s -p ${worker} ${worker}/run.py --coordinator ${cfg.coordinator} --token ${cfg.token}
-          '';
-          environment = {
-            LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${worker}/__pypackages__/3.10/lib/numpy.libs/:${worker}/__pypackages__/3.10/lib/tokenizers.libs/";
-            MODELS_DIR = cfg.modelsDir;
-          };
-          path = [ pkgs.ffmpeg.bin ];
-          wantedBy = [ "multi-user.target" ];
-        };
-      }
-      )
+  config = mkIf cfg.enable {
+    systemd.services.transcribee-worker = {
+      enable = true;
+      unitConfig = {
+        Type = "simple";
+        Restart = "always";
+      };
+      script = ''
+        ${pkgs.pdm}/bin/pdm run -s -p ${worker} ${worker}/run.py --coordinator ${cfg.coordinator} --token ${cfg.token}
+      '';
+      environment = {
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${worker}/__pypackages__/3.10/lib/numpy.libs/:${worker}/__pypackages__/3.10/lib/tokenizers.libs/";
+        MODELS_DIR = cfg.modelsDir;
+      };
+      path = [ pkgs.ffmpeg.bin ];
+      wantedBy = [ "multi-user.target" ];
+    };
 
-      (optionalAttrs (hasAttr "launchd" options) {
-        launchd.daemons.transcribee-worker = {
-          script = ''
-            ${pkgs.pdm}/bin/pdm run -s -p ${worker} ${worker}/run.py --coordinator ${cfg.coordinator} --token ${cfg.token}
-          '';
-          environment = {
-            LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${worker}/__pypackages__/3.10/lib/numpy.libs/:${worker}/__pypackages__/3.10/lib/tokenizers.libs/";
-            MODELS_DIR = cfg.modelsDir;
-          };
-          path = [
-            pkgs.ffmpeg.bin
-            pkgs.pkg-config
-          ];
-          serviceConfig = {
-            KeepAlive = true;
-            RunAtLoad = true;
-            StandardOutPath = "/var/log/transcribee-worker.log";
-            StandardErrorPath = "/var/log/transcribee-worker.log";
-          };
-        };
-      })
-    ]);
+    launchd.daemons.transcribee-worker = {
+      script = ''
+        ${pkgs.pdm}/bin/pdm run -s -p ${worker} ${worker}/run.py --coordinator ${cfg.coordinator} --token ${cfg.token}
+      '';
+      environment = {
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${worker}/__pypackages__/3.10/lib/numpy.libs/:${worker}/__pypackages__/3.10/lib/tokenizers.libs/";
+        MODELS_DIR = cfg.modelsDir;
+      };
+      path = [
+        pkgs.ffmpeg.bin
+        pkgs.pkg-config
+      ];
+      serviceConfig = {
+        KeepAlive = true;
+        RunAtLoad = true;
+        StandardOutPath = "/var/log/transcribee-worker.log";
+        StandardErrorPath = "/var/log/transcribee-worker.log";
+      };
+    };
+  };
 }
