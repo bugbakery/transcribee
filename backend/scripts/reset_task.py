47f9821c8156c380341d5a9867cb707ad0f837a2
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2024-01-07 21:19:46 +0100

    backend: allow reset_task script to reset tasks with any state

--- backend/scripts/reset_task.py
+++ backend/scripts/reset_task.py
@@ -11,13 +11,20 @@ if __name__ == "__main__":
     parser.add_argument(
         "--uuid", required=True, type=uuid.UUID, help="Task UUID or Document UUID"
     )
+    parser.add_argument(
+        "--state",
+        type=TaskState,
+        choices=list(TaskState),
+        default=TaskState.FAILED,
+        help="State of tasks to reset",
+    )
     args = parser.parse_args()
     with SessionContextManager(path="management_command:reset_task") as session:
         task = session.execute(
             update(Task)
             .where(
                 or_(Task.id == args.uuid, Task.document_id == args.uuid),
-                Task.state == TaskState.FAILED,
+                Task.state == args.state,
             )
             .values(state=TaskState.NEW, remaining_attempts=settings.task_attempt_limit)
         )
