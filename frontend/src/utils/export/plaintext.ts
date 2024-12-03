0e36b6804bbd6e093a0c7112616b305796b5cb19
Jaro Habiger <jarohabiger@googlemail.com>
2024-12-03 23:17:48 +0000

    ✨ add option for timestamps in plaintext export

--- frontend/src/utils/export/plaintext.ts
+++ frontend/src/utils/export/plaintext.ts
@@ -1,18 +1,38 @@
+import { formattedTime } from '../../editor/transcription_editor';
 import { Document } from '../../editor/types';
 import { getSpeakerName } from '../document';
 
-export function generatePlaintext(doc: Document, includeSpeakerNames: boolean): string {
+export function generatePlaintext(
+  doc: Document,
+  includeSpeakerNames: boolean,
+  includeTimestamps: boolean,
+): string {
   let last_speaker: string | null = null;
   return doc.children
     .map((paragraph) => {
       let paragraphText = '';
+      if (
+        last_speaker !== null &&
+        ((includeSpeakerNames && last_speaker !== paragraph.speaker) ||
+          (includeTimestamps && !includeSpeakerNames))
+      ) {
+        paragraphText += '\n';
+      }
+      if (includeTimestamps && (last_speaker !== paragraph.speaker || !includeSpeakerNames)) {
+        paragraphText += `[${formattedTime(paragraph.children[0].start)}]\n`;
+      }
       if (includeSpeakerNames && last_speaker !== paragraph.speaker) {
         paragraphText += `${getSpeakerName(paragraph.speaker, doc.speaker_names)}:\n`;
-        last_speaker = paragraph.speaker;
       }
-      paragraphText += paragraph.children.map((x) => x.text).join('');
-      return paragraphText.trim();
+
+      paragraphText += paragraph.children
+        .map((x) => x.text)
+        .join('')
+        .trim();
+
+      last_speaker = paragraph.speaker;
+      return paragraphText;
     })
     .filter((x) => x !== '')
-    .join('\n\n');
+    .join('\n');
 }
