385e81adaadf6cea817effc7ad1a0316a6d6828e
Philipp Mandler <info@philipp-mandler.com>
2025-01-08 00:03:47 +0100

    worker: Switch to uv and make nix derivation pure

--- nix/pkgs/worker.nix
+++ nix/pkgs/worker.nix
@@ -1,83 +1,55 @@
-{ lib
-, pkgs
-, python3 ? pkgs.python311
-, ...
+{
+  pkgs,
+  lib,
+  python,
+  uv2nix,
+  pyproject-nix,
+  pyproject-build-systems,
+  system,
+  ...
 }:
-with lib;
 let
-  pyprojectInfo = builtins.fromTOML (builtins.readFile ../../worker/pyproject.toml);
-  pdmFixedPkgs = (import (builtins.fetchTarball {
-    name = "nixos-unstable-with-fixed-pdm";
-    url = "https://github.com/nixos/nixpkgs/archive/9482c3b0cffed8365f686c22c83df318b4473a3e.tar.gz";
-    sha256 = "05rgyl1i09jzsvhwg3blvac7x9mayj3kqpp55h287qxsimsslh0x";
-  }) {});
-in
-python3.pkgs.buildPythonApplication rec {
-  pname = pyprojectInfo.project.name;
-  version = pyprojectInfo.project.version;
-  src = ../..;
-  pyproject = true;
+  workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ../../worker; };
 
-  # expose this because we want to use the same version when using `pdm run` externally to run this package
-  pdm = pdmFixedPkgs.pdm;
+  overlay = workspace.mkPyprojectOverlay {
+    sourcePreference = "wheel";
+  };
 
-  nativeBuildInputs = [
-    pdmFixedPkgs.pdm
-    pdmFixedPkgs.python3.pkgs.pdm-pep517
-    pkgs.git
-    pkgs.cacert
-    pkgs.maturin
-    pkgs.cargo
-    pkgs.rustc
-    pkgs.ffmpeg
-    pkgs.pkg-config
-    pkgs.icu
-    pkgs.icu.dev
-  ];
+  pyprojectOverrides = final: prev: {
+    automerge = prev.automerge.overrideAttrs (old: {
+      nativeBuildInputs = old.nativeBuildInputs ++ [
+        pkgs.rustPlatform.cargoSetupHook
+        pkgs.rustPlatform.maturinBuildHook
+      ];
 
-  buildInputs = with pkgs;[
-    libiconv
+      cargoDeps = pkgs.rustPlatform.importCargoLock {
+        lockFile = "${old.src}/Cargo.lock";
+      };
+    });
 
-    # for automerge-py
-    rustc
-    cargo
-    maturin
-    clangStdenv
+    pyicu = prev.pyicu.overrideAttrs (old: {
+      nativeBuildInputs = old.nativeBuildInputs ++ [
+        pkgs.icu.dev
+        (final.resolveBuildSystem {})
+      ];
+    });
+  };
 
-    icu.dev
+  pythonSet =
+    (pkgs.callPackage pyproject-nix.build.packages {
+      inherit python;
+      stdenv = pkgs.stdenv.override {
+        targetPlatform = pkgs.stdenv.targetPlatform // {
+          darwinSdkVersion = "13.0";
+        };
+      };
+    }).overrideScope
+      (
+        lib.composeManyExtensions [
+          pyproject-build-systems.overlays.default
+          overlay
+          pyprojectOverrides
+        ]
+      );
 
-    # provides libmagic
-    file
-  ] ++
-  # accelerates whisper.cpp on M{1,2} Macs
-  (lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Accelerate);
-
-
-  propagatedBuildInputs = with pkgs;
-    [
-      ffmpeg
-    ];
-
-
-  doCheck = false;
-  dontCheckRuntimeDeps = true;
-
-  configurePhase = ''
-    cd worker/
-  '';
-
-  installPhase = pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
-    export PYICU_CFLAGS="-I${pkgs.libcxx.dev}/include/c++/v1"
-  ''
-  + ''
-    export PDM_TMP=$(mktemp -d)
-    export PDM_CONFIG_FILE=$PDM_TMP/config
-    export CARGO_HOME=$(mktemp -d)
-
-    pdm config cache_dir $PDM_TMP/cache
-    TERM=dumb pdm install --no-lock --check --prod
-
-    mkdir -p $out
-    cp -r * .* $out/
-  '';
-}
+in pythonSet.mkVirtualEnv "transcribee-worker-env" workspace.deps.default
