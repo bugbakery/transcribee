05967e0e740ad97c91aa08936d5eb3b799d02840
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2025-02-17 19:09:40 +0100

    dev: fix docker setup

--- packaging/setup_backend.sh
+++ packaging/setup_backend.sh
@@ -3,7 +3,9 @@
 set -euxo pipefail
 
 echo -e "\033[1m# setting up backend:\033[0m\n"
-wait4x postgresql "postgres://@/transcribee?host=$(pwd)/../backend/db/sockets/"
+if [[ -z "TRANSCRIBEE_BACKEND_DATABASE_URL" ]]; then
+  wait4x postgresql "postgres://@/transcribee?host=$(pwd)/../backend/db/sockets/"
+fi
 poe -C ../backend/ migrate
-poe -C ../backend/ create_user --user test --pass test
-poe -C ../backend/ create_worker --token dev_worker --name "Development Worker"
+poe -C ../backend/ admin create_user --user test --pass test
+poe -C ../backend/ admin create_worker --token dev_worker --name "Development Worker"
