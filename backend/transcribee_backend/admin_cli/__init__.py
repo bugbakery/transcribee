797890992b9c81af35b86cc408835b0ea41e2e54
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2025-02-27 12:27:35 +0100

    admin: add set_document script

--- backend/transcribee_backend/admin_cli/__init__.py
+++ backend/transcribee_backend/admin_cli/__init__.py
@@ -6,10 +6,11 @@ from .commands.create_user import CreateUserCmd
 from .commands.create_user_token import CreateUserTokenCmd
 from .commands.create_worker import CreateWorkerCmd
 from .commands.reset_task import ResetTaskCmd
+from .commands.set_document import SetDocumentCmd
 from .commands.set_password import SetPasswordCmd
 
 parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(metavar="COMMAND")
+subparsers = parser.add_subparsers(required=True, metavar="COMMAND")
 
 
 def add_command(name: str, description: str, command: Command):
@@ -25,6 +26,7 @@ add_command("create_user", "Create a new user", CreateUserCmd())
 add_command("create_worker", "Register a new worker", CreateWorkerCmd())
 add_command("reset_task", "Reset a task", ResetTaskCmd())
 add_command("set_password", "Set the password of a user", SetPasswordCmd())
+add_command("set_document", "Set the document contents of a document", SetDocumentCmd())
 
 
 def main():
