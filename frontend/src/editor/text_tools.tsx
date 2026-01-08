6fad5ae6c91b26ef24e1cfce42005d75c42ceeac
Jaro Habiger <jarohabiger@googlemail.com>
2026-01-08 01:42:56 +0100

    ✨ add smart reflow

--- frontend/src/editor/text_tools.tsx
+++ frontend/src/editor/text_tools.tsx
@@ -15,6 +15,33 @@ export const MenuItemButton = primitiveWithClassname('button', [
   'block',
 ]);
 
+function mergeSameSpeakerParagraphs(doc: Document) {
+  const mergePoints: number[] = [];
+  for (let i = 0; i < doc.children.length - 1; i++) {
+    const paragraph = doc.children[i];
+    const nextParagraph = doc.children[i + 1];
+    if (paragraph.speaker == nextParagraph.speaker) {
+      mergePoints.push(i);
+    }
+  }
+  let removed = 0;
+  mergePoints.forEach((index) => {
+    const i = index - removed;
+    doc.children[i].children.push(...JSON.parse(JSON.stringify(doc.children[i + 1].children)));
+    doc.children.splice(i + 1, 1);
+    removed++;
+  });
+}
+
+const punctuations = ['.', '?', '!'];
+const non_punctuations = ['...'];
+function containsSentenceEnd(text: string) {
+  return (
+    punctuations.some((punct) => text.includes(punct)) &&
+    !non_punctuations.some((np) => text.includes(np))
+  );
+}
+
 export function TextTools({ editor }: { editor: EditorWithWebsocket }) {
   return (
     <Popup
@@ -25,25 +52,7 @@ export function TextTools({ editor }: { editor: EditorWithWebsocket }) {
     >
       <MenuItemButton
         onClick={() => {
-          const mergePoints: number[] = [];
-          for (let i = 0; i < editor.doc.children.length - 1; i++) {
-            const paragraph = editor.doc.children[i];
-            const nextParagraph = editor.doc.children[i + 1];
-            if (paragraph.speaker == nextParagraph.speaker) {
-              mergePoints.push(i);
-            }
-          }
-          editor.update((doc: Document) => {
-            let removed = 0;
-            mergePoints.forEach((index) => {
-              const i = index - removed;
-              doc.children[i].children.push(
-                ...JSON.parse(JSON.stringify(doc.children[i + 1].children)),
-              );
-              doc.children.splice(i + 1, 1);
-              removed++;
-            });
-          });
+          editor.update(mergeSameSpeakerParagraphs);
         }}
       >
         Reflow to One Paragraph per Speaker
@@ -51,36 +60,11 @@ export function TextTools({ editor }: { editor: EditorWithWebsocket }) {
 
       <MenuItemButton
         onClick={() => {
-          const punctuations = ['.', '?', '!'];
-          const non_punctuations = ['...'];
-          const contains_punctuation = (text: string) =>
-            punctuations.some((punct) => text.includes(punct)) &&
-            !non_punctuations.some((np) => text.includes(np));
-
-          // stategy: we first merge everything that could possibly be merged...
-          const mergePoints: number[] = [];
-          for (let i = 0; i < editor.doc.children.length - 1; i++) {
-            const paragraph = editor.doc.children[i];
-            const nextParagraph = editor.doc.children[i + 1];
-            if (
-              !contains_punctuation(paragraph.children[paragraph.children.length - 1].text) &&
-              paragraph.speaker == nextParagraph.speaker
-            ) {
-              mergePoints.push(i);
-            }
-          }
           editor.update((doc: Document) => {
-            let removed = 0;
-            mergePoints.forEach((index) => {
-              const i = index - removed;
-              doc.children[i].children.push(
-                ...JSON.parse(JSON.stringify(doc.children[i + 1].children)),
-              );
-              doc.children.splice(i + 1, 1);
-              removed++;
-            });
+            // stategy: we first merge everything that could possibly be merged...
+            mergeSameSpeakerParagraphs(doc);
 
-            // ...and only then break up
+            // ...and only then break up on sentence boundaries
             const newChildren: Paragraph[] = [];
             doc.children.forEach((paragraph) => {
               let currentParagraph = {
@@ -89,7 +73,7 @@ export function TextTools({ editor }: { editor: EditorWithWebsocket }) {
               };
               paragraph.children.forEach((token) => {
                 currentParagraph.children.push(JSON.parse(JSON.stringify(token)));
-                if (contains_punctuation(token.text)) {
+                if (containsSentenceEnd(token.text)) {
                   newChildren.push(currentParagraph);
                   currentParagraph = {
                     ...paragraph,
@@ -106,6 +90,92 @@ export function TextTools({ editor }: { editor: EditorWithWebsocket }) {
         }}
       >
         Reflow to One Paragraph per Sentence
+      </MenuItemButton>
+
+      <MenuItemButton
+        onClick={() => {
+          // this strategy tries to split paragraphs at sentence boundaries, but only if there is a pause between the sentences
+          // or the paragraphs would become too long.
+          const initial = 2;
+          const decay = 0.95;
+
+          const getPause = (i: number, paragraph: Paragraph) => {
+            const token = paragraph.children[i];
+            const nextToken = paragraph.children[i + 1];
+            if (nextToken?.start !== undefined && token?.end !== undefined) {
+              return nextToken.start - token.end;
+            }
+            return 0;
+          };
+
+          editor.update((doc: Document) => {
+            mergeSameSpeakerParagraphs(doc);
+            const newChildren: Paragraph[] = [];
+            const addNewChild = (paragraph: Paragraph) => {
+              // if the paragraph is very long and does not contain any sentence ends, we still want to break it up
+              if (paragraph.children.length <= 100) {
+                newChildren.push(paragraph);
+              } else {
+                const silences = paragraph.children
+                  .map((x, i) => ({ ...x, pause: getPause(i, paragraph) }))
+                  .filter((token) => token.text.includes(','))
+                  .map((token) => token.pause);
+                silences.sort();
+                const thresholdIndex = Math.floor(paragraph.children.length / 100); // aim for paragraphs of max ~50 tokens
+                const threshold = silences[silences.length - 1 - thresholdIndex];
+                let currentParagraph = {
+                  ...paragraph,
+                  children: [] as { text: string }[],
+                };
+                paragraph.children.forEach((token, i) => {
+                  currentParagraph.children.push(JSON.parse(JSON.stringify(token)));
+                  if (
+                    getPause(i, paragraph) >= threshold &&
+                    token.text.includes(',') &&
+                    currentParagraph.children.length > 3
+                  ) {
+                    newChildren.push(currentParagraph);
+                    currentParagraph = {
+                      ...paragraph,
+                      children: [],
+                    };
+                  }
+                });
+                if (currentParagraph.children.length > 0) {
+                  newChildren.push(currentParagraph);
+                }
+              }
+            };
+            doc.children.forEach((paragraph) => {
+              let minPauseBetweenSentences = initial; // this gets reduced with every additional token
+              let currentParagraph = {
+                ...paragraph,
+                children: [] as { text: string }[],
+              };
+              paragraph.children.forEach((token, i) => {
+                currentParagraph.children.push(JSON.parse(JSON.stringify(token)));
+                minPauseBetweenSentences *= decay;
+                if (
+                  getPause(i, paragraph) >= minPauseBetweenSentences &&
+                  containsSentenceEnd(token.text)
+                ) {
+                  addNewChild(currentParagraph);
+                  minPauseBetweenSentences = initial;
+                  currentParagraph = {
+                    ...paragraph,
+                    children: [],
+                  };
+                }
+              });
+              if (currentParagraph.children.length > 0) {
+                addNewChild(currentParagraph);
+              }
+            });
+            doc.children = newChildren;
+          });
+        }}
+      >
+        Smart Reflow ✨
       </MenuItemButton>
     </Popup>
   );
