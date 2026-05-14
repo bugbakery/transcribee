be2f6ad498a18233c1d11b78ac82b450a55eb109
Philipp Mandler <info@philipp-mandler.com>
2026-05-14 23:52:34 +0200

    Add initial desktop application

---
+++ desktop/src-tauri/src/lib.rs
@@ -0,0 +1,43 @@
+use tauri::{Manager, path::BaseDirectory};
+use tauri_plugin_shell::ShellExt;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn ffmpeg_help(app_handle: tauri::AppHandle) -> Result<String, String> {
+    let resource_path = app_handle.path().resolve(
+        "native-deps/bin/ffmpeg",
+        BaseDirectory::Resource,
+    ).map_err(|e| { e.to_string() })?;
+    println!("{:?}", resource_path);
+
+    let shell = app_handle.shell();
+    let output = tauri::async_runtime::block_on(async move {
+        shell
+            .command(resource_path)
+            .args(["--help"])
+            .output()
+            .await
+            .unwrap()
+    });
+
+    if output.status.success() {
+        Ok(format!("Result: {}", String::from_utf8(output.stdout).unwrap()))
+    } else {
+        Err(format!("Exit with code: {}", output.status.code().unwrap()))
+    }
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet, ffmpeg_help])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
