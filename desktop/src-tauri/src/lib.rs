4aef129670b181f3a95857b30a4ad98e3d039767
Jaro Habiger <jarohabiger@googlemail.com>
2026-05-16 20:45:56 +0200

    run worker in tauri app & add more beautiful logging

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,4 +1,9 @@
+use colored::Color;
+use log::Level;
+use tauri_plugin_log::fern;
 use tauri_plugin_shell::ShellExt;
+
+mod worker_plugin;
 
 #[tauri::command]
 fn ffmpeg_help(app_handle: tauri::AppHandle) -> Result<String, String> {
@@ -13,7 +18,10 @@ fn ffmpeg_help(app_handle: tauri::AppHandle) -> Result<String, String> {
     });
 
     if output.status.success() {
-        Ok(format!("Result: {}", String::from_utf8(output.stdout).unwrap()))
+        Ok(format!(
+            "Result: {}",
+            String::from_utf8(output.stdout).unwrap()
+        ))
     } else {
         Err(format!("Exit with code: {}", output.status.code().unwrap()))
     }
@@ -22,6 +30,27 @@ fn ffmpeg_help(app_handle: tauri::AppHandle) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(tauri_plugin_log::log::LevelFilter::Info)
+                .format(|callback: fern::FormatCallback, message, record| {
+                    let mut color = match record.metadata().target() {
+                        "worker" => Color::Blue,
+                        _ => Color::Black,
+                    };
+                    if record.metadata().level() == Level::Error {
+                        color = Color::Red;
+                    }
+
+                    callback.finish(format_args!(
+                        "{color_line}{target: <8}| {message}\x1B[0m",
+                        color_line = format_args!("\x1B[{}m", color.to_fg_str()),
+                        target = record.target(),
+                        message = message,
+                    ))
+                })
+                .build(),
+        )
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![ffmpeg_help])
