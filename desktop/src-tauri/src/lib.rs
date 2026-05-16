86709591002382dd21cb2d6f227794e0c3b23457
Jaro Habiger <jarohabiger@googlemail.com>
2026-05-16 20:45:56 +0200

    beautify tauri logs

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,5 +1,10 @@
-use tauri::{Manager, path::BaseDirectory};
+use colored::Color;
+use log::Level;
+use tauri::{path::BaseDirectory, Manager};
+use tauri_plugin_log::fern;
 use tauri_plugin_shell::ShellExt;
+
+mod worker_plugin;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -9,10 +14,10 @@ fn greet(name: &str) -> String {
 
 #[tauri::command]
 fn ffmpeg_help(app_handle: tauri::AppHandle) -> Result<String, String> {
-    let resource_path = app_handle.path().resolve(
-        "native-deps/bin/ffmpeg",
-        BaseDirectory::Resource,
-    ).map_err(|e| { e.to_string() })?;
+    let resource_path = app_handle
+        .path()
+        .resolve("native-deps/bin/ffmpeg", BaseDirectory::Resource)
+        .map_err(|e| e.to_string())?;
     println!("{:?}", resource_path);
 
     let shell = app_handle.shell();
@@ -26,7 +31,10 @@ fn ffmpeg_help(app_handle: tauri::AppHandle) -> Result<String, String> {
     });
 
     if output.status.success() {
-        Ok(format!("Result: {}", String::from_utf8(output.stdout).unwrap()))
+        Ok(format!(
+            "Result: {}",
+            String::from_utf8(output.stdout).unwrap()
+        ))
     } else {
         Err(format!("Exit with code: {}", output.status.code().unwrap()))
     }
@@ -35,8 +43,30 @@ fn ffmpeg_help(app_handle: tauri::AppHandle) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(tauri_plugin_log::log::LevelFilter::Info)
+                .format(|callback: fern::FormatCallback, message, record| {
+                    let mut color = match record.metadata().target() {
+                        "worker" => Color::Blue,
+                        _ => Color::Black,
+                    };
+                    if record.metadata().level() == Level::Error {
+                        color = Color::Red;
+                    }
+
+                    callback.finish(format_args!(
+                        "{color_line}{target: <8}| {message}\x1B[0m",
+                        color_line = format_args!("\x1B[{}m", color.to_fg_str()),
+                        target = record.target(),
+                        message = message,
+                    ))
+                })
+                .build(),
+        )
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(worker_plugin::init())
         .invoke_handler(tauri::generate_handler![greet, ffmpeg_help])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
