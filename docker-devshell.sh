269859a4861f35251c5b9a284fbf1c6a2a65cc18
Philipp Mandler <info@philipp-mandler.com>
2026-05-18 00:21:09 +0200

    Ditch docker-devshell.sh in favor of dev container

--- docker-devshell.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -ouxe pipefail
-docker build . -f packaging/docker/Dockerfile-dev --tag transcribee-dev
-docker run --rm -ti -v $(pwd):/workspace -p 5173:5173 transcribee-dev
