c21859748a642b442c5bda5b1b3809ebe9fd9bb6
Philipp Mandler <info@philipp-mandler.com>
2026-06-03 17:58:27 +0200

    Add worker to docker compose

--- packaging/docker/run.sh
+++ packaging/docker/run.sh
@@ -8,6 +8,10 @@ if [ ! -z "$TESTUSER_PASSWORD" ]; then
   poe admin create_user --user test --pass "$TESTUSER_PASSWORD"
 fi
 
+if [ ! -z "$DEFAULT_WORKER_TOKEN" ]; then
+  poe admin create_worker --token "$DEFAULT_WORKER_TOKEN" --name "Default Worker"
+fi
+
 uvicorn transcribee_backend.main:app \
   --host "0.0.0.0" \
   --ws websockets \
