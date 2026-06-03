5590afc4ffac6a035067430439cb9ab04f438998
Jaro Habiger <jarohabiger@googlemail.com>
2026-06-03 21:45:25 +0200

    use randomly generated worker tokens in docker setup

--- packaging/docker/run.sh
+++ packaging/docker/run.sh
@@ -8,8 +8,10 @@ if [ ! -z "$TESTUSER_PASSWORD" ]; then
   poe admin create_user --user test --pass "$TESTUSER_PASSWORD"
 fi
 
-if [ ! -z "$DEFAULT_WORKER_TOKEN" ]; then
-  poe admin create_worker --token "$DEFAULT_WORKER_TOKEN" --name "Default Worker"
+if  [ ! -z "$WORKER_TOKEN_PATH" ] && [ ! -f $WORKER_TOKEN_PATH ]; then
+  WORKER_TOKEN=$(tr -dc 'A-Za-z0-9' < /dev/random | head -c32 || true)
+  poe admin create_worker --token "$WORKER_TOKEN" --name "Default Docker Worker"
+  echo -n $WORKER_TOKEN > $WORKER_TOKEN_PATH
 fi
 
 uvicorn transcribee_backend.main:app \
