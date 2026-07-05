b981bb912afc6425daf00ea2d646ab70bdf88d70
Philipp Mandler <info@philipp-mandler.com>
2026-07-05 18:05:29 +0200

    docker: allow WORKER_TOKEN_PATH to contain spaces

--- packaging/docker/run.sh
+++ packaging/docker/run.sh
@@ -8,10 +8,10 @@ if [ ! -z "$TESTUSER_PASSWORD" ]; then
   poe admin create_user --user test --pass "$TESTUSER_PASSWORD"
 fi
 
-if  [ ! -z "$WORKER_TOKEN_PATH" ] && [ ! -f $WORKER_TOKEN_PATH ]; then
+if  [ ! -z "$WORKER_TOKEN_PATH" ] && [ ! -f "$WORKER_TOKEN_PATH" ]; then
   WORKER_TOKEN=$(tr -dc 'A-Za-z0-9' < /dev/random | head -c32 || true)
   poe admin create_worker --token "$WORKER_TOKEN" --name "Default Docker Worker"
-  echo -n $WORKER_TOKEN > $WORKER_TOKEN_PATH
+  echo -n $WORKER_TOKEN > "$WORKER_TOKEN_PATH"
 fi
 
 uvicorn transcribee_backend.main:app \
