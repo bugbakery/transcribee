8455581cbb7ab1c37189e1c084b42b221e32e985
Jaro Habiger <jarohabiger@googlemail.com>
2026-07-13 15:38:07 +0200

    frontend: bump automerge version

--- frontend/src/document.ts
+++ frontend/src/document.ts
@@ -3,8 +3,18 @@ import { next as Automerge } from '@automerge/automerge';
 import * as AutomergeStable from '@automerge/automerge';
 
 function convertString(s: string): string {
-  // this typecasting is a hack to avoid having multiple types for different versions for now
-  return new AutomergeStable.Text(s.toString()) as unknown as string;
+  // previously we added some fields as automerge immutable strings. Since version 3.0 the automerge
+  // js api treats js strings as collaborative text. Reading, calling .toString() on a member and
+  // writing it back converts it to a collaborative text.
+  return s.toString();
+}
+
+function ensureFloatChild<T>(obj: T, member: keyof T) {
+  // this is only working in the specific context of obj being an automerge document.
+  // because the automerge js api gives every number as a number. Reading and writing it back
+  // causes the number to become a float.
+  const v = obj[member];
+  obj[member] = v;
 }
 
 export function migrateDocument(doc: Automerge.Doc<Document>): Automerge.Doc<Document> {
@@ -27,16 +37,13 @@ export function migrateDocument(doc: Automerge.Doc<Document>): Automerge.Doc<Doc
         doc.children.forEach((paragraph) => {
           paragraph.type = convertString(paragraph.type) as 'paragraph';
           paragraph.speaker = paragraph.speaker ? convertString(paragraph.speaker) : null;
-          paragraph.lang = convertString(paragraph.lang.toString());
+          paragraph.lang = convertString(paragraph.lang);
 
           paragraph.children.forEach((child) => {
-            const start = child.start;
-            child.start = start;
-            const end = child.end;
-            child.end = end;
-            const conf = child.conf;
-            child.conf = conf;
-            child.text = convertString(child.text.toString());
+            ensureFloatChild(child, 'start');
+            ensureFloatChild(child, 'end');
+            ensureFloatChild(child, 'conf');
+            child.text = convertString(child.text);
           });
         });
         doc.version = 2;
