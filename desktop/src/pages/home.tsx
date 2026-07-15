588f68b5f75c0e3e4ca52ca3fbbfbaab679967a8
Jaro Habiger <jarohabiger@googlemail.com>
2026-07-15 15:55:38 +0200

    add barebones transcribee desktop frontend

---
+++ desktop/src/pages/home.tsx
@@ -0,0 +1,8 @@
+import { PrimaryButton, SecondaryButton } from 'transcribee-ui-common/components/button'
+
+export function HomePage(props: {}) {
+    return <div className='flex flex-col w-full h-full items-center justify-center gap-4'>
+        <PrimaryButton className='block w-60'>Transcribe Audio</PrimaryButton>
+        <SecondaryButton className='block w-60'>Open Transcribed File</SecondaryButton>
+    </div>
+}
---
+++ desktop/tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', '../ui-common/{components,editor,utils}/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        brutal: '5px 5px #000',
+      },
+      keyframes: {
+        rainbow: {
+          '0%': { color: '#db0000' },
+          '15%': { color: '#f1b500' },
+          '45%': { color: '#00bd2c' },
+          '60%': { color: '#00b9b3' },
+          '75%': { color: '#0091ff' },
+          '90%': { color: '#b405a5' },
+          '100%': { color: '#db0000' },
+        },
+      },
+      animation: {
+        rainbow: 'rainbow 6s linear infinite;',
+        'spin-slow': 'spin 1s linear infinite',
+      },
+      width: { 'very-large-dialog': '60rem' },
+    },
+    fontFamily: {
+      sans: [
+        'Inter',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Noto Sans',
+        'Helvetica',
+        'Arial',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+      ],
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
