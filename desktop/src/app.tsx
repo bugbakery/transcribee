02fdc240d7b0121c5418025a741cc005854b1d43
Jaro Habiger <jarohabiger@googlemail.com>
2026-07-15 15:55:38 +0200

    add barebones transcribee desktop frontend

---
+++ desktop/src/app.tsx
@@ -0,0 +1,15 @@
+import { Route, Router, } from 'wouter';
+import { ModalHolder } from 'transcribee-ui-common/components/modal'
+import { HomePage } from './pages/home';
+
+
+function App() {
+  return (
+    <Router>
+      <ModalHolder />
+        <Route path="/" component={HomePage} />
+    </Router>
+  );
+}
+
+export default App;
