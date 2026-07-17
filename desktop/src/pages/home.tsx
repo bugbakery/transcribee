b56eaea527bc43b570a63dc98a8f59df6f0f8831
Philipp Mandler <info@philipp-mandler.com>
2026-07-17 15:42:28 +0200

    desktop: Add linting and formatting checks

--- desktop/src/pages/home.tsx
+++ desktop/src/pages/home.tsx
@@ -1,8 +1,10 @@
-import { PrimaryButton, SecondaryButton } from 'transcribee-ui-common/components/button'
+import { PrimaryButton, SecondaryButton } from 'transcribee-ui-common/components/button';
 
-export function HomePage(props: {}) {
-    return <div className='flex flex-col w-full h-full items-center justify-center gap-4'>
-        <PrimaryButton className='block w-60'>Transcribe Audio</PrimaryButton>
-        <SecondaryButton className='block w-60'>Open Transcribed File</SecondaryButton>
+export function HomePage() {
+  return (
+    <div className="flex flex-col w-full h-full items-center justify-center gap-4">
+      <PrimaryButton className="block w-60">Transcribe Audio</PrimaryButton>
+      <SecondaryButton className="block w-60">Open Transcribed File</SecondaryButton>
     </div>
+  );
 }
