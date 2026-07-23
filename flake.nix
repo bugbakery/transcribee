ef5de944db3e2dc4e806e4dcb32a8d03ddc241b8
Jaro Habiger <jarohabiger@googlemail.com>
2026-07-23 12:39:53 +0200

    bump nix flake

--- flake.nix
+++ flake.nix
@@ -3,34 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-
-    pyproject-nix = {
-      url = "github:pyproject-nix/pyproject.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    uv2nix = {
-      url = "github:pyproject-nix/uv2nix";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    pyproject-build-systems = {
-      url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       nixpkgs,
-      uv2nix,
-      pyproject-nix,
-      pyproject-build-systems,
       flake-utils,
       self,
       ...
