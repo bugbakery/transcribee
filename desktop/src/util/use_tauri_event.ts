cd0a787ee135b17c64355bd85844bd88bed790a4
Philipp Mandler <info@philipp-mandler.com>
2026-08-11 21:46:08 +0200

    Fix potential race condition in useTauriState

--- desktop/src/util/use_tauri_event.ts
+++ desktop/src/util/use_tauri_event.ts
@@ -8,13 +8,11 @@ import { useEffect, useState } from 'react';
 export function useTauriState<T>(getFn: () => Promise<T>, event: string, initial: T): T {
   const [state, setState] = useState(initial);
   useEffect(() => {
-    const unlisten = { current: () => {} };
-    (async () => {
-      setState(await getFn());
-      unlisten.current = await listen<T>(event, (e) => setState(e.payload));
-    })();
+    getFn().then((res) => setState(res));
+    const unlistenPromise = listen<T>(event, (e) => setState(e.payload));
+
     return () => {
-      unlisten.current();
+      unlistenPromise.then((unlisten) => unlisten());
     };
   }, [event]);
   return state;
