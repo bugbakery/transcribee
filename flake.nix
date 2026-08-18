94aa988e10abdc858fc0bf08284cf563e085ee8d
Jaro Habiger <jarohabiger@googlemail.com>
2026-08-18 18:17:47 +0200

    desktop: fix media plaback on linux

--- flake.nix
+++ flake.nix
@@ -90,6 +90,10 @@
             pkgs.libsoup_3
             pkgs.webkitgtk_4_1
             pkgs.pkg-config
+            pkgs.gst_all_1.gstreamer
+            pkgs.gst_all_1.gst-plugins-base
+            pkgs.gst_all_1.gst-plugins-good
+            pkgs.gst_all_1.gst-plugins-bad
           ];
 
           RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
@@ -120,6 +124,7 @@
               _doit
               export RUSTFLAGS="-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold";
               export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS"
+              export GST_PLUGIN_SYSTEM_PATH_1_0="${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0/:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0/:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0/:${pkgs.gst_all_1.gstreamer.out}/lib/gstreamer-1.0/"
             '';
         };
       }
