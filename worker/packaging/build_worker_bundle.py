6dab14af8b0c2f89afd662f557304137054b9bd0
Philipp Mandler <info@philipp-mandler.com>
2026-08-18 15:28:22 +0200

    Download bundled python via uv and fix installation on windows

--- worker/packaging/build_worker_bundle.py
+++ worker/packaging/build_worker_bundle.py
@@ -4,6 +4,7 @@
 import argparse
 import hashlib
 import json
+import shutil
 import tarfile
 import tempfile
 import urllib.request
@@ -85,8 +86,7 @@ def extract_native_dep_tar(file: Path, target_path: Path):
                 merge_into(child, target_path)
         else:
             raise Exception(
-                "Unsupported archive layout."
-                "Expecting either one toplevel file or one toplevel dir."
+                "Unsupported archive layout.Expecting either one toplevel file or one toplevel dir."
             )
 
 
@@ -113,6 +113,70 @@ def download_native_deps(native_deps_json: Path, platform: str, target: Path):
 
             extract_native_dep_tar(dl_path, target)
     return target
+
+
+def download_python(folder: Path, platform: str, target: Path):
+    with tempfile.TemporaryDirectory() as tempdir:
+        platform_map = {
+            "darwin-aarch64": "macos-aarch64-none",
+            "darwin-x86_64": "macos-x86_64-none",
+            "linux-aarch64": "linux-aarch64-gnu",
+            "linux-x86_64": "linux-x86_64-gnu",
+            "win-x86_64": "windows-x86_64-none",
+        }
+
+        python_version = None
+        with open(folder / ".python-version") as pv_file:
+            python_version = pv_file.readline().strip()
+        print(f"-> using python version {python_version}")
+
+        python_target = f"cpython-{python_version}-" + platform_map[platform]
+
+        cmd = [
+            "uv",
+            "python",
+            "install",
+            "--managed-python",  # do not use system python
+            "--no-bin",  # do not add to systems bin directory
+            "--install-dir",
+            str(tempdir),
+            python_target,
+        ]
+        print("-> running", " ".join(cmd))
+        check_call(cmd, cwd=folder)
+
+        # The following lines are more thought-out than you might think. UV behaves a bit weird
+        # here. It won't create links for version aliases when downloading python for windows on
+        # OSes other than windows. So we might not get a folder (or link) matching the name of our
+        # requested target. Nor can we assume to only get one non-symlink installation, since when
+        # running on windows the aliases are created but are no symlinks.
+        #
+        # The naive approach of picking the first installation and resolving the potential symlink
+        # works here because we download to a clean directory and all folders living here should be
+        # the same installation we just downloaded.
+        python_dir = next(Path(tempdir).glob("cpython-*"))
+        shutil.move(python_dir.resolve(), target)
+
+        return target
+
+
+def find_site_packages(python_folder: Path):
+    windows_path = python_folder / "Lib" / "site-packages"
+    if windows_path.exists():
+        return windows_path
+
+    unix_path = next(python_folder.glob("lib/python3.*/site-packages"), None)
+    if unix_path is not None:
+        return unix_path
+
+    raise Exception(f"Could not find site_packages in {python_folder}")
+
+
+def install_venv(python_folder: Path, venv_folder):
+    print("-> populating site_packages")
+    target_site_packages = find_site_packages(python_folder)
+    venv_site_packages = find_site_packages(venv_folder)
+    shutil.copytree(venv_site_packages, target_site_packages, dirs_exist_ok=True)
 
 
 def prepare_venv(folder: Path, platform: str, target: Path):
@@ -195,6 +259,11 @@ if __name__ == "__main__":
             rmtree(venv_folder, ignore_errors=True)
             prepare_venv(worker_dir, platform, venv_folder)
 
+            python_folder = Path(tempdir) / "python"
+            rmtree(python_folder, ignore_errors=True)
+            download_python(worker_dir, platform, python_folder)
+            install_venv(python_folder, venv_folder)
+
             native_deps_folder = Path(tempdir) / "native_deps"
             rmtree(native_deps_folder, ignore_errors=True)
             download_native_deps(
@@ -212,11 +281,11 @@ if __name__ == "__main__":
                     Path(__file__).parent / f"run_worker.{ext}.tmpl",
                     f"run_worker.{ext}",
                 )
-                print("-> packing venv")
-                archive.add(venv_folder / "lib", "lib")
+                print("-> packing python")
+                archive.add(python_folder, "python")
                 print("-> packing native deps")
                 archive.add(native_deps_folder, "")
             if not force_rebuild:
                 source_hash_file.write_text(current_src_digest)
 
-            print("-> done\n")
+            print("-> done")
