2bc39db16f51ef310f058e2b15562be3631cfec0
Jaro Habiger <jarohabiger@googlemail.com>
2026-08-21 18:27:48 +0200

    desktop: log to console under windows
    this is a bit of a hack but still better than no output at all

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -2,5 +2,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // stolen from https://github.com/tauri-apps/tauri/issues/8305#issuecomment-1826871949
+    #[cfg(windows)]
+    {
+        use windows::Win32::System::Console::{AttachConsole, ATTACH_PARENT_PROCESS};
+        // we ignore the result here because
+        // if the app started from a command line, like cmd or powershell,
+        // it will attach sucessfully which is what we want
+        // but if we were started from something like explorer,
+        // it will fail to attach console which is also what we want.
+        let _ = unsafe { AttachConsole(ATTACH_PARENT_PROCESS) };
+    }
     transcribee_desktop_lib::run()
 }
