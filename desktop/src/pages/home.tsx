7b3a50390b0dc6249d608a756af498fb0f1c43db
Jaro Habiger <jarohabiger@googlemail.com>
2026-08-21 18:46:31 +0200

    desktop: add padding to startup modal

--- desktop/src/pages/home.tsx
+++ desktop/src/pages/home.tsx
@@ -31,6 +31,7 @@ export function HomePage() {
   useEffect(() => {
     showModal(
       <DoubleWidthModal
+        dialogClassName="m-4"
         onClose={() => showModal(null)}
         label="Welcome to the First Alpha Version of Transcribee-Desktop"
       >
